6a7a9acf83137a8f1f11632fe81ee53ae1ac7e11
marwaw <martyna.waw96@gmail.com>
2018-06-19 13:26:10 +0200

    move read file to utils

--- zad1.py
+++ zad1.py
@@ -1,9 +1,6 @@
-import pandas as pd
 import numpy as np
 
-
-def read_csv(file_name):
-    return pd.read_csv(file_name, sep=',', header=0).values
+from utils import read_csv
 
 
 def sprint_planning_helper(file, velocity):
